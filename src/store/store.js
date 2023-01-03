64124209a0ee69038df3146330ee33c543116fa4
Panji <panji9502@gmail.com>
2023-01-03 20:52:03 +0700

    renaming redux saga

--- src/store/store.js
+++ src/store/store.js
@@ -6,13 +6,16 @@ import {
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "@redux-saga/core";
 
 import { rootReducer } from "./root-reducer";
+import { rootSaga } from "./root-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const middleWares = [
   process.env.NODE_ENV === "development" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -35,4 +38,6 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
